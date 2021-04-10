5c28482c1bdb7f59f1df529e7b6ecd609366528c
John Davenport <johns10davenport@gmail.com>
2021-04-10 10:36:04 -0400

    Add failing test

--- test/hero_test.exs
+++ test/hero_test.exs
@@ -5,4 +5,8 @@ defmodule HeroTest do
   test "greets the world" do
     assert Hero.hello() == :world
   end
+
+  test "fails to greet the heavens" do
+    assert Hero.hello() == :heavens
+  end
 end
